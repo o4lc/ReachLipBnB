9557b36c583e4e87750643135446103794c7db02
sinnna <sharifi.sina1377@gmail.com>
2022-08-27 19:35:32 -0400

    solving\finding the rotated rectangle with pca for one step

--- run.py
+++ run.py
@@ -6,11 +6,13 @@ from packages import *
 from BranchAndBound import BranchAndBound
 from NeuralNetwork import NeuralNetwork
 import pandas as pd
+from sklearn.decomposition import PCA
+
 torch.set_printoptions(precision=8)
 
 def main():
 
-    eps = .0001
+    eps = .01
     verbose = 0
     virtualBranching = False
     numberOfVirtualBranches = 4,
@@ -42,7 +44,6 @@ def main():
     network = NeuralNetwork(pathToStateDictionary)
 
 
-    # @TODO: Input dimension and Output dimension are not necessarily the same!
     dim = network.Linear[0].weight.shape[1]
     outputDim = network.Linear[-1].weight.shape[0]
     network.to(device)
@@ -51,53 +52,50 @@ def main():
     upperCoordinate = torch.Tensor([1., 1.]).to(device)
     c = torch.Tensor([1., 2.]).to(device)
 
-    # lowerCoordinate = torch.Tensor([torch.pi / 3, torch.pi / 3, torch.pi / 3]).to(device)
-    # upperCoordinate = torch.Tensor([2 * torch.pi / 3, 2 * torch.pi / 3, 2 * torch.pi / 3]).to(device)
-
-    # if "ACAS" in pathToStateDictionary or "mnist" in pathToStateDictionary:
-
-    # lowerCoordinate = torch.Tensor([-2./2560] * dim).to(device)
-    # upperCoordinate = torch.Tensor([2./2560] * dim).to(device)
-    # lowerCoordinate = torch.Tensor([-1.] * dim).to(device)
-    # upperCoordinate = torch.Tensor([1.] * dim).to(device)
-    # c = torch.ones(outputDim).to(device)
-    # c = torch.zeros(outputDim, dtype=torch.float).to(device)
-    if "mnist" in pathToStateDictionary:
-        df = pd.read_csv("mnistTestData.csv")
-        testImage = torch.Tensor(df.loc[0].to_numpy()[1:]/255.).to(device)
-        lowerCoordinate += testImage
-        upperCoordinate += testImage
-        testLabel = df.loc[0].label
-        c[testLabel] = 1.
-        try:
-            c[testLabel + 1] = -1
-        except:
-            c[testLabel - 1] = -1
-    else:
-        c[0] = 1.
-        c[1] = -1
-
     startTime = time.time()
-    BB = BranchAndBound(upperCoordinate, lowerCoordinate, verbose=verbose, inputDimension=dim, eps=eps, network=network,
-                        queryCoefficient=c, device=device, nodeBranchingFactor=2, branchNodeNum=512,
-                        scoreFunction='length',
-                        pgdIterNum=0, pgdNumberOfInitializations=2, pgdStepSize=0.5, virtualBranching=virtualBranching,
-                        numberOfVirtualBranches=numberOfVirtualBranches,
-                        maxSearchDepthLipschitzBound=maxSearchDepthLipschitzBound,
-                        normToUseLipschitz=normToUseLipschitz, useTwoNormDilation=useTwoNormDilation,
-                        useSdpForLipschitzCalculation=useSdpForLipschitzCalculation,
-                        lipschitzSdpSolverVerbose=lipschitzSdpSolverVerbose,
-                        initialGD = False
-                        )
-    lowerBound, upperBound, space_left = BB.run()
-    endTime = time.time()
-
-    if verbose:
-        print(BB)
     
-    print(' ')
-    print('Best lower/upper bounds are:', lowerBound, '->' ,upperBound)
+    inputData = (upperCoordinate - lowerCoordinate) * torch.rand(100, dim, device=device) \
+             + lowerCoordinate
+
+    inputData = Variable(inputData, requires_grad=False)
+    with no_grad():
+        imageData = network.forward(inputData)
+
+    pca = PCA()
+    pcaData = pca.fit_transform(imageData)
+
+    data_mean = pca.mean_
+    data_comp = pca.components_
+
+    pcaDirections = []
+    for direction in data_comp:
+        pcaDirections.append(direction)
+        pcaDirections.append(-direction)
+    pcaDirections = torch.Tensor(np.array(pcaDirections))
+    
+    for c in pcaDirections:
+        print('** Solving with coefficient =', c)
+        BB = BranchAndBound(upperCoordinate, lowerCoordinate, verbose=verbose, inputDimension=dim, eps=eps, network=network,
+                            queryCoefficient=c, device=device, nodeBranchingFactor=2, branchNodeNum=512,
+                            scoreFunction='length',
+                            pgdIterNum=0, pgdNumberOfInitializations=2, pgdStepSize=0.5, virtualBranching=virtualBranching,
+                            numberOfVirtualBranches=numberOfVirtualBranches,
+                            maxSearchDepthLipschitzBound=maxSearchDepthLipschitzBound,
+                            normToUseLipschitz=normToUseLipschitz, useTwoNormDilation=useTwoNormDilation,
+                            useSdpForLipschitzCalculation=useSdpForLipschitzCalculation,
+                            lipschitzSdpSolverVerbose=lipschitzSdpSolverVerbose,
+                            initialGD = False
+                            )
+        lowerBound, upperBound, space_left = BB.run()
+
+        print(' ')
+        print('Best lower/upper bounds are:', lowerBound, '->' ,upperBound)
+
+
+    endTime = time.time()
+    
     print('The algorithm took (s):', endTime - startTime, 'with eps =', eps)
+
 
 
 if __name__ == '__main__':
